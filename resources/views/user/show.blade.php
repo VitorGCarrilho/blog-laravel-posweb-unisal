0f5078435efed3516d6390684a8c0d8e410c8ecc
VitorGCarrilho <vitorg.carrilho@gmail.com>
2017-06-28 23:01:03 -0300

    Fixing bug on call wrong controller

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -25,7 +25,7 @@
 		</ul>
 
 		<span class="mdl-list__item-secondary-content">
-			{{ link_to_action('EntryController@edit','Edit', $author->id,array('class' => 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent')) }}
+			{{ link_to_action('UserController@edit','Edit', $author->id,array('class' => 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent')) }}
 		</span>
 	</div>
 	<div class="mdl-cell mdl-cell--6-col">
